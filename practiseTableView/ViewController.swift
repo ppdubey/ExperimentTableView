35fd50fdc9d4d675758e86e72666dc9f386993a9
ppd <priya242@gmail.com>
2021-06-02 04:57:22 -0700

    adding custom cell by making view from xib

--- practiseTableView/ViewController.swift
+++ practiseTableView/ViewController.swift
@@ -12,7 +12,11 @@ class ViewController: UIViewController {
     
     @IBOutlet var tableView: UIView!
     
-    var names  = [ 10, 20, 55  ]
+    var names  = [ 10, 20, 55, 10, 20, 55, 10, 20, 55,
+                   10, 20, 55, 10, 20, 55, 10, 20, 55, 10, 20, 55, 10, 20, 55, 10, 20, 55, 10, 20, 55,
+                   10, 20, 55,10, 20, 55,10, 20, 55,
+                   10, 20, 55,10, 20, 55,10, 20, 55,
+                   10, 20, 55,10, 20, 55,10, 20, 55]
     
     
     override func viewDidLoad() {
@@ -28,10 +32,15 @@ extension ViewController : UITableViewDelegate, UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         print(" selectec items : \(names[indexPath.row])")
+        tableView.cellForRow(at: indexPath)?.backgroundColor = UIColor.blue
+    }
+    
+    func tableView(_ tableView: UITableView, didDeselectRowAt indexPath: IndexPath) {
+        tableView.cellForRow(at: indexPath)?.backgroundColor = nil
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "myCell", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "myCellFromXib", for: indexPath)
         
         cell.textLabel?.text = String(names[indexPath.row])
         
---
+++ practiseTableView/myCellClassTableViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  myCellClassTableViewCell.swift
+//  practiseTableView
+//
+//  Created by user196667 on 6/1/21.
+//
+
+import UIKit
+
+class myCellClassTableViewCell: UITableViewCell {
+
+     
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        //print("set selectec: \(selected)")
+        // Configure the view for the selected state
+
+    }
+    
+    
+}
